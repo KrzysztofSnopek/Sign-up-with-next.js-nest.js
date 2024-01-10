0979c133ee28f24eecd6cf7c7639f5e66267e50a
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2024-01-10 22:28:13 +0100

    Refactors layout for responsiveness, replace inputs with Material UI

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,43 +1,97 @@
+"use client";
+
+import { TextField } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1C2E24",
+    },
+  },
+});
+const accent = "#1C2E24";
+
 export default function LoginPage() {
   return (
-    <div className="w-full h-screen bg-neutral-50 justify-center items-start inline-flex">
-      <div className="w-1/3 self-stretch px-3 pt-[319px] pb-[287px] bg-neutral-800 flex-col justify-end items-center inline-flex">
-        <div className="self-stretch h-[376px] p-6 flex-col justify-center items-start inline-flex">
-          <div className="self-stretch h-[328px] pt-12 flex-col justify-start items-start flex">
-            <div className="w-[432px] text-neutral-50 text-5xl font-normal font-['Roboto Slab'] leading-[56.02px]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    <div className="w-full h-screen flex bg-neutral-50">
+      {/* Welcome board - left content - make reusable for sign in form */}
+      <div className="w-1/3 h-screen px-3 bg-neutral-800 justify-end items-center flex">
+        <div className="px-6 pt-12">
+          <div className="text-neutral-50 text-5xl font-normal font-['Roboto Slab'] leading-[56.02px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </div>
+          <div className="w-4 bg-white bg-opacity-0 flex-col justify-start items-start flex">
+            <div className="w-4 h-8 relative bg-white bg-opacity-0">
+              <div className="w-4 h-8 left-0 top-0 absolute"></div>
             </div>
-            <div className="w-4 bg-white bg-opacity-0 flex-col justify-start items-start flex">
-              <div className="w-4 h-8 relative bg-white bg-opacity-0">
-                <div className="w-4 h-8 left-0 top-0 absolute"></div>
-              </div>
-            </div>
-            <div className="self-stretch text-neutral-50 text-base font-normal font-['Roboto'] leading-normal tracking-tight">
-              — John Doe
-            </div>
+          </div>
+          <div className="text-neutral-50 text-base font-normal font-['Roboto'] leading-normal tracking-tight">
+            — John Doe
           </div>
         </div>
       </div>
-      <div className="grow shrink basis-0 self-stretch justify-center items-center inline-flex">
-        <div className="grow shrink basis-0 self-stretch p-6 flex-col justify-center items-center inline-flex">
-          <div className="h-[354px] flex-col justify-start items-start flex">
-            <div className="h-[92px] flex-col justify-start items-start flex">
-              <div className="self-stretch justify-start items-start inline-flex">
-                <div className="grow shrink basis-0 flex-col justify-start items-start inline-flex">
-                  <div className="self-stretch text-center text-black text-opacity-90 text-2xl font-normal font-['Roboto Slab'] leading-loose">
+
+      {/* Right content - form part */}
+      <div className="w-2/3 h-screen flex justify-center items-center">
+        <div className="w-3/5 p-6">
+          <div className="">
+            <div className="h-[92px]">
+              {/* Welcome msg */}
+              <div className="">
+                <div className="">
+                  <div className="text-center text-black/90 text-2xl font-normal font-['Roboto Slab'] leading-loose">
                     Welcome
                   </div>
                 </div>
               </div>
-              <div className="self-stretch justify-start items-start inline-flex">
-                <div className="grow shrink basis-0 text-center text-black text-opacity-60 text-base font-normal font-['Roboto'] leading-7 tracking-tight">
+              <div className="">
+                <div className="text-center text-black/60 text-base font-normal font-['Roboto'] leading-7 tracking-tight">
                   Example Description
                 </div>
               </div>
-              <div className="h-8 bg-white bg-opacity-0 flex-col justify-start items-start flex">
-                <div className="w-8 h-8 relative bg-white bg-opacity-0">
-                  <div className="w-8 h-6 left-0 top-[4px] absolute"></div>
-                </div>
+            </div>
+
+            {/* Space above input - padding */}
+            <div className="h-4 bg-white bg-opacity-0 flex-col justify-start items-start flex">
+              <div className="w-8 h-4 relative bg-white bg-opacity-0">
+                <div className="w-8 h-4 left-0 top-0 absolute"></div>
+              </div>
+            </div>
+
+            {/* Email input */}
+            <div className="self-stretch h-14 gap-[3px]">
+              <TextField
+                fullWidth
+                id="outlined-basic"
+                label="Email address"
+                variant="outlined"
+                color="primary"
+              />
+            </div>
+
+            {/* Space between inputs - padding */}
+            <div className="h-4 bg-white bg-opacity-0 flex-col justify-start items-start flex">
+              <div className="w-8 h-4 relative bg-white bg-opacity-0">
+                <div className="w-8 h-4 left-0 top-0 absolute"></div>
+              </div>
+            </div>
+
+            {/* Password input */}
+            <div className="self-stretch h-14 gap-[3px]">
+              <TextField
+                fullWidth
+                id="outlined-basic"
+                label="Password"
+                variant="outlined"
+                color="primary"
+              />
+            </div>
+
+            {/* 2x space under password input - padding */}
+            <div className="h-4 bg-white bg-opacity-0 flex-col justify-start items-start flex">
+              <div className="w-8 h-4 relative bg-white bg-opacity-0">
+                <div className="w-8 h-4 left-0 top-0 absolute"></div>
               </div>
             </div>
             <div className="h-4 bg-white bg-opacity-0 flex-col justify-start items-start flex">
@@ -45,49 +99,8 @@ export default function LoginPage() {
                 <div className="w-8 h-4 left-0 top-0 absolute"></div>
               </div>
             </div>
-            <div className="self-stretch h-14 flex-col justify-start items-start gap-[3px] flex">
-              <div className="self-stretch h-14 px-3 rounded border border-black border-opacity-25 flex-col justify-start items-start flex">
-                <div className="self-stretch py-4 justify-start items-center gap-2 inline-flex">
-                  <div className="grow shrink basis-0 text-black text-opacity-90 text-base font-normal font-['Roboto'] leading-normal tracking-tight">
-                    Value
-                  </div>
-                </div>
-                <div className="h-0.5 px-1 bg-white justify-start items-center gap-2.5 inline-flex">
-                  <div className="text-black text-opacity-60 text-xs font-normal font-['Roboto'] leading-3 tracking-tight">
-                    Email address
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="h-4 bg-white bg-opacity-0 flex-col justify-start items-start flex">
-              <div className="w-8 h-4 relative bg-white bg-opacity-0">
-                <div className="w-8 h-4 left-0 top-0 absolute"></div>
-              </div>
-            </div>
-            <div className="self-stretch h-14 flex-col justify-start items-start gap-[3px] flex">
-              <div className="self-stretch h-14 px-3 rounded border border-black border-opacity-25 flex-col justify-start items-start flex">
-                <div className="self-stretch py-4 justify-start items-center gap-2 inline-flex">
-                  <div className="grow shrink basis-0 text-black text-opacity-90 text-base font-normal font-['Roboto'] leading-normal tracking-tight">
-                    **********
-                  </div>
-                </div>
-                <div className="h-0.5 px-1 bg-white justify-start items-center gap-2.5 inline-flex">
-                  <div className="text-black text-opacity-60 text-xs font-normal font-['Roboto'] leading-3 tracking-tight">
-                    Password
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="h-4 bg-white bg-opacity-0 flex-col justify-start items-start flex">
-              <div className="w-8 h-4 relative bg-white bg-opacity-0">
-                <div className="w-8 h-4 left-0 top-0 absolute"></div>
-              </div>
-            </div>
-            <div className="h-4 bg-white bg-opacity-0 flex-col justify-start items-start flex">
-              <div className="w-8 h-4 relative bg-white bg-opacity-0">
-                <div className="w-8 h-4 left-0 top-0 absolute"></div>
-              </div>
-            </div>
+
+            {/* Sign in button */}
             <div className="self-stretch h-[50px] bg-neutral-800 rounded shadow flex-col justify-center items-center flex">
               <div className="px-[22px] py-3 justify-center items-center gap-2 inline-flex">
                 <div className="text-white text-[15px] font-medium font-['Roboto'] uppercase leading-relaxed tracking-wide">
@@ -95,16 +108,22 @@ export default function LoginPage() {
                 </div>
               </div>
             </div>
+
+            {/* Space under sign in button - padding */}
             <div className="h-4 bg-white bg-opacity-0 flex-col justify-start items-start flex">
               <div className="w-8 h-4 relative bg-white bg-opacity-0">
                 <div className="w-8 h-4 left-0 top-0 absolute"></div>
               </div>
             </div>
-            <div className="self-stretch h-5 flex-col justify-center items-center flex">
+
+            {/* Sign up footer */}
+            <div className="self-stretch h-5 justify-center flex">
               <div>
                 <span className="text-black text-opacity-90 text-sm font-normal font-['Roboto'] leading-tight tracking-tight">
                   Don't have an account?
                 </span>
+
+                {/* button for sign up */}
                 <span className="text-black text-opacity-90 text-sm font-bold font-['Roboto'] leading-tight tracking-tight">
                   Sign up for free
                 </span>
